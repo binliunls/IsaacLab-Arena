ae498fd7c4b71351becde61f983793b826d7f6c9
vramasamy <vramasamy@nvidia.com>
2025-07-08 10:49:08 +0200

    Get gui working for isaaclab

--- docker/run_and_push_docker.sh
+++ docker/run_and_push_docker.sh
@@ -7,9 +7,25 @@ SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
 
 push=false
 
+# Default mount directory on the host machine for the datasets
+DATASETS_HOST_MOUNT_DIRECTORY="$HOME/datasets"
+# Default mount directory on the host machine for the models
+MODELS_HOST_MOUNT_DIRECTORY="$HOME/models"
+# Default mount directory on the host machine for the evaluation directory
+EVAL_HOST_MOUNT_DIRECTORY="$HOME/eval"
+
 while getopts ":hptn:" OPTION; do
     case $OPTION in
 
+        d)
+            DATASETS_HOST_MOUNT_DIRECTORY=$OPTARG
+            ;;
+        m)
+            MODELS_HOST_MOUNT_DIRECTORY=$OPTARG
+            ;;
+        e)
+            EVAL_HOST_MOUNT_DIRECTORY=$OPTARG
+            ;;
         p)
             push=true
             ;;
@@ -51,4 +67,36 @@ if $push ; then
     docker push $DOCKER_IMAGE_NAME:${TAG}
 fi
 
-docker run -it -e "ACCEPT_EULA=Y" ${DOCKER_IMAGE_NAME}:${TAG}
+DOCKER_RUN_ARGS=("--privileged"
+                "--ulimit" "memlock=-1"
+                "--ulimit" "stack=-1"
+                "--ipc=host"
+                "--net=host"
+                "--runtime=nvidia"
+                "--gpus=all"
+                "-v" ".:/workspaces/isaac_arena"
+                "-v" "$DATASETS_HOST_MOUNT_DIRECTORY:/datasets"
+                "-v" "$MODELS_HOST_MOUNT_DIRECTORY:/models"
+                "-v" "$EVAL_HOST_MOUNT_DIRECTORY:/eval"
+                "-v" "$HOME/.bash_history:/home/$(id -un)/.bash_history"
+                "-v" "$HOME/.config/osmo:/home/$(id -un)/.config/osmo"
+                "-v" "/tmp:/tmp"
+                "-v" "/tmp/.X11-unix:/tmp/.X11-unix:rw"
+                "-v" "/var/run/docker.sock:/var/run/docker.sock"
+                "-v" "$HOME/.Xauthority:/root/.Xauthority"
+                "--env" "DISPLAY"
+                "--env" "ACCEPT_EULA=Y"
+                "--env" "PRIVACY_CONSENT=Y"
+                "--env" "DOCKER_RUN_USER_ID=$(id -u)"
+                "--env" "DOCKER_RUN_USER_NAME=$(id -un)"
+                "--env" "DOCKER_RUN_GROUP_ID=$(id -g)"
+                "--env" "DOCKER_RUN_GROUP_NAME=$(id -gn)"
+                "--env" "OMNI_USER=\$omni-api-token"
+                "--env" "OMNI_PASS=$OMNI_PASS"
+                "--env" "OMNI_KIT_ALLOW_ROOT=1"
+                )
+
+# Allow X11 connections
+xhost +local:docker
+
+docker run "${DOCKER_RUN_ARGS[@]}" --interactive --rm --tty ${DOCKER_IMAGE_NAME}:${TAG}
